b76810cc932fee869db5667413d661a5f6e85b99
Tate Mioton <tatemioton@outlook.com>
2023-08-25 23:38:43 -0500

    Use Godot Changes

--- open-simplex-noise.h
+++ open-simplex-noise.h
@@ -21,31 +21,38 @@
  */
 
 #if ((__GNUC_STDC_INLINE__) || (__STDC_VERSION__ >= 199901L))
-	#include <stdint.h>
+    #include <stdint.h>
 	#define INLINE inline
 #elif (defined (_MSC_VER) || defined (__GNUC_GNU_INLINE__))
-	#include <stdint.h>
-	#define INLINE __inline
-#else 
-	/* ANSI C doesn't have inline or stdint.h. */
+    #include <stdint.h>
+    #define INLINE __inline
+#else
+    /* ANSI C doesn't have inline or stdint.h. */
 	#define INLINE
 #endif
 
 #ifdef __cplusplus
-	extern "C" {
+extern "C" {
 #endif
 
-struct osn_context;
+// -- GODOT start --
+// Modified to work without allocating memory, also removed some unused function.
 
-int open_simplex_noise(int64_t seed, struct osn_context **ctx);
+struct osn_context {
+    int16_t perm[256];
+    int16_t permGradIndex3D[256];
+};
+
+int open_simplex_noise(int64_t seed, struct osn_context *ctx);
+//int open_simplex_noise_init_perm(struct osn_context *ctx, int16_t p[], int nelements);
+// -- GODOT end --
 void open_simplex_noise_free(struct osn_context *ctx);
-int open_simplex_noise_init_perm(struct osn_context *ctx, int16_t p[], int nelements);
 double open_simplex_noise2(const struct osn_context *ctx, double x, double y);
 double open_simplex_noise3(const struct osn_context *ctx, double x, double y, double z);
 double open_simplex_noise4(const struct osn_context *ctx, double x, double y, double z, double w);
 
 #ifdef __cplusplus
-	}
+}
 #endif
 
 #endif
